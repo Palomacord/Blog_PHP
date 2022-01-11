10a88a7a4703a201810889a37a71d7e96987579c
Palomacord <84478374+Palomacord@users.noreply.github.com>
2022-01-10 21:22:20 -0300

    first commit

---
+++ 15_blog/contatos.php
@@ -0,0 +1,10 @@
+<?php
+include_once ("templates/header.php")
+?>
+
+
+   <h3>pagina de contato</h3>
+
+<?php
+include_once ("templates/footer.php")
+?>
---
+++ 15_blog/templates/footer.php
@@ -0,0 +1,11 @@
+<?php
+include_once("helpers/url.php");
+?>
+
+
+<footer>
+    <p> Hora de codar&copy; 2022</p>
+</footer>
+
+
+</html>
